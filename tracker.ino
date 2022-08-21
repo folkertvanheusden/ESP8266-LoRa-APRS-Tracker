8273ea37581dd1e09a68eb44dcfc85db33c35ae2
folkert van heusden <mail@vanheusden.com>
2022-08-21 09:51:32 +0200

    sync word is 0x12 for lora-aprs(?)

--- tracker.ino
+++ tracker.ino
@@ -40,7 +40,7 @@ void setup() {
 	LoRa.setSignalBandwidth(125000);
 	LoRa.setCodingRate4(5);
 	LoRa.setPreambleLength(8);
-	LoRa.setSyncWord(0x34);
+	LoRa.setSyncWord(0x12);
 	LoRa.enableCrc();
 
 	LoRa.setTxPower(20);
